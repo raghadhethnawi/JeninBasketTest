7740f6be0600d82ad48836d2fa91daf39a6e5afc
Raghad Hethnawi <raghadhethnawi@gmail.com>
2020-12-04 17:19:20 +0200

    Updated Dependencies

--- app/src/main/java/com/example/jeninbasket/MainActivity.java
+++ app/src/main/java/com/example/jeninbasket/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.jeninbasket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.jeninbasket.Adapters.PlateAdapter;
+import com.example.jeninbasket.Models.PlateModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<PlateModel> plateModelList;
+    private PlateAdapter plateAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setKeepScreenOn(true);
+        recyclerView.setHasFixedSize(true);
+
+        plateModelList = new ArrayList<>();
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+
+        plateAdapter = new PlateAdapter(plateModelList, this);
+        recyclerView.setAdapter(plateAdapter);
+        plateAdapter.notifyDataSetChanged();
     }
 }
